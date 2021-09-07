6a3c5e29ae7b1ef525b651cd626a8b83a649845c
Bonface Mwicwiri <software6@varaluae.com>
2021-09-08 01:14:06 +0300

    07/09/2021 working on models and admin

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,102 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import Place, Route, Airline, Check, Plane, Flight, Booking
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'updated', 'created')
+    search_fields = ('name',)
+    list_filter = ('updated', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+
+class AirlineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'updated', 'created')
+    search_fields = ('name',)
+    list_filter = ('updated', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('code', 'source', 'destination', 'updated', 'created')
+    search_fields = ('code',)
+    list_filter = ('updated', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+
+class PlaneAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'airline', 'updated', 'created')
+    search_fields = ('code',)
+    list_filter = ('updated', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('code', 'plane', 'seats_no', 'arrival', 'route', 'price', 'updated', 'created')
+    search_fields = ('code',)
+    list_filter = ('updated', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('code', 'flight', 'passenger', 'updated', 'created')
+    search_fields = ('code',)
+    list_filter = ('updated', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Airline, AirlineAdmin)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Plane, PlaneAdmin)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Check)
