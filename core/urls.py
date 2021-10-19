bec274b5fb793efd53f27e83054a3eabc3539c1c
Bonface Mwicwiri <software6@varaluae.com>
2021-10-20 00:56:54 +0300

    19/10/2021 working passenger

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('finance/', include(('finance.urls', 'finance'), namespace="finance")),
     path('', include(('passenger.urls', 'passenger'), namespace="passenger")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    template_name = "finance/index.html"
+
+    def get(self):
+        return render(self.request, self.template_name)
+
+    def post(self):
+        pass
+
+
+class LoginView(View):
+    template_name = "finance/auth/sign-in.html"
+
+    def get(self):
+        return render(self.request, self.template_name)
+
+    def post(self):
+        pass
+
+
+class SignUpView(View):
+    template_name = "finance/auth/sign-up.html"
+
+    def get(self):
+        return render(self.request, self.template_name)
+
+    def post(self):
+        pass
+
+
+class FormView(View):
+    template_name = "finance/basic-form-elements.html"
+
+    def get(self):
+        return render(self.request, self.template_name)
+
+    def post(self):
+        pass
+
+
+class ContactView(View):
+    template_name = "finance/contact.html"
+
+    def get(self):
+        return render(self.request, self.template_name)
+
+    def post(self):
+        pass
+
+
+class TableView(View):
+    template_name = "finance/jquery-datatable.html"
+
+    def get(self):
+        return render(self.request, self.template_name)
+
+    def post(self):
+        pass
+
