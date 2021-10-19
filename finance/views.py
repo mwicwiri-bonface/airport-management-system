30ada8d4af5b24e6608a25139ddd5e03b173d20c
Bonface Mwicwiri <software6@varaluae.com>
2021-10-20 01:00:32 +0300

    19/10/2021 working passenger

--- finance/views.py
+++ finance/views.py
@@ -5,59 +5,59 @@ from django.views import View
 class HomeView(View):
     template_name = "finance/index.html"
 
-    def get(self):
+    def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
 
 
 class LoginView(View):
     template_name = "finance/auth/sign-in.html"
 
-    def get(self):
+    def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
 
 
 class SignUpView(View):
     template_name = "finance/auth/sign-up.html"
 
-    def get(self):
+    def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
 
 
 class FormView(View):
     template_name = "finance/basic-form-elements.html"
 
-    def get(self):
+    def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
 
 
 class ContactView(View):
     template_name = "finance/contact.html"
 
-    def get(self):
+    def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
 
 
 class TableView(View):
     template_name = "finance/jquery-datatable.html"
 
-    def get(self):
+    def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
 
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
 
