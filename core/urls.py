95c1813c5a426957e9d1156c173862ecdd080b87
Bonface Mwicwiri <software6@varaluae.com>
2021-10-27 16:59:20 +0300

    27/10/2021 Working on passenger

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # password reset
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name="accounts/password-reset/password_reset_form.html",
+             html_email_template_name="accounts/password-reset/email_reset_template.html",
+             subject_template_name="accounts/password-reset/password_reset_subject.txt",
+         ),
+         name="password_reset"),
+    path('reset_password_done/',
+         auth_views.PasswordResetDoneView.as_view(template_name="accounts/password-reset/password_reset_done.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="accounts/password-reset/password_reset_confirm.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="accounts/password-reset/password_reset_complete.html"),
+         name="password_reset_complete"),
+
     path('admin/', admin.site.urls),
     path('finance/', include(('finance.urls', 'finance'), namespace="finance")),
     path('', include(('passenger.urls', 'passenger'), namespace="passenger")),
